642c21bede05f4d5149397eba9d2bf0cec328147
Lars Müller <mryellowhd@gmail.com>
2017-08-08 17:07:08 +0200

    / Git

---
+++ src/main/java/de/nomizon/justclick/main/JustClick.java
@@ -0,0 +1,9 @@
+package de.nomizon.justclick.main;
+
+/**
+ * Erstellt von Lars am 08.08.2017.
+ * Alle Rechte vorbehalten. Der Entwickler kann jederzeit
+ * die Rechte an diesem Code entziehen!
+ */
+public class JustClick {
+}
