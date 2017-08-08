12e36e322cc8d21b2ed530448da4dd60805dc949
Lars Müller <mryellowhd@gmail.com>
2017-08-08 17:09:32 +0200

    + de.nomizon.justlick.main.JustClick

--- src/main/java/de/nomizon/justclick/main/JustClick.java
+++ src/main/java/de/nomizon/justclick/main/JustClick.java
@@ -1,9 +1,22 @@
 package de.nomizon.justclick.main;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Erstellt von Lars am 08.08.2017.
  * Alle Rechte vorbehalten. Der Entwickler kann jederzeit
  * die Rechte an diesem Code entziehen!
  */
-public class JustClick {
+public class JustClick extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
